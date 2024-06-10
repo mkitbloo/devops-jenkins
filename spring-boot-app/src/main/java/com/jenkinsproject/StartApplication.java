4431fe807bb3155c6ccbddbc5193955ac1bcf4fe
Marie <mariechoi430@gmail.com>
2024-06-10 14:59:04 +1000

    Initial commit of java-app

---
+++ spring-boot-app/src/main/java/com/jenkinsproject/StartApplication.java
@@ -0,0 +1,24 @@
+package com.jenkinsproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+@Controller
+public class StartApplication {
+
+    @GetMapping("/")
+    public String index(final Model model) {
+        model.addAttribute("title", "Successfully built a spring boot application using Maven");
+        model.addAttribute("msg", "This application is deployed on to Kubernetes using Argo CD for Jenkins DevOps pipeline project");
+        return "index";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(StartApplication.class, args);
+    }
+
+}
